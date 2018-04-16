1b6386bf498495ac9920c8edcdf10f4f786b5165
Vinicius B. Dias <vinicius@ANAJUSTRACBA.CORP>
2018-04-16 18:25:10 -0300

    Commit até aula 02, atividade 08.

--- javascript-avancado-i-aula1/aluraframe/client/js/app/models/Negociacao.js
+++ javascript-avancado-i-aula1/aluraframe/client/js/app/models/Negociacao.js
@@ -1,9 +1,32 @@
 class Negociacao {
 
-    constructor() {
+    constructor(data, quantidade, valor) {
 
-        this.data = new Date();
-        this.quantidade = 1;
-        this.valor = 0.0;
+        this._data = data;
+        this._quantidade = quantidade;
+        this._valor = valor;
+
+        Object.freeze(this);
     }
+
+    get volume() {
+
+        return this._quantidade * this._valor;
+    }
+
+    get data() {
+
+        return new Date(this._data.getTime());
+    }
+
+    get quantidade() {
+
+        return this._quantidade;
+    }
+
+    get valor() {
+
+        return this._valor;
+    }
+
 }
