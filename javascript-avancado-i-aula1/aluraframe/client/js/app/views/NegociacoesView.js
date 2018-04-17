08b583431d5cf18577c5749c7f00ed02eb1cc222
Vinícius Dias <vintheman@gmail.com>
2018-04-17 19:24:58 -0300

    Commit até aula 06, exercício 01.

--- javascript-avancado-i-aula1/aluraframe/client/js/app/views/NegociacoesView.js
+++ javascript-avancado-i-aula1/aluraframe/client/js/app/views/NegociacoesView.js
@@ -19,21 +19,25 @@ class NegociacoesView {
             </thead>
 
             <tbody>
-                ${model.negociacoes.map(n => {
-                    return `
+                ${model.negociacoes.map(n =>
+                    `
                     <tr>
                         <td>${DateHelper.dataParaTexto(n.data)}</td>
                         <td>${n.quantidade}</td>
                         <td>${n.valor}</td>
                         <td>${n.volume}</td>
                     </tr>
-                    `;
-                }).join('')}
+                    `
+                ).join('')}
             </tbody>
 
             <tfoot>
+                <td colspan="3"></td>
+                <td>
+                    ${model.negociacoes.reduce((total, n) => total + n.volume, 0.0)}
+                </td>
             </tfoot>
-        </table>        
+        </table>
         `;
     }
 
