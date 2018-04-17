0ee36cae0321292628ff05552f885f41d2bfe495
Vinicius B. Dias <vinicius@ANAJUSTRACBA.CORP>
2018-04-17 15:46:49 -0300

    Commit até aula 05, atividade 02.

--- javascript-avancado-i-aula1/aluraframe/client/js/app/controllers/NegociacaoController.js
+++ javascript-avancado-i-aula1/aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -7,24 +7,30 @@ class NegociacaoController {
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
         this._listaNegociacoes = new ListaNegociacoes();
+        this._negociacoesView = new NegociacoesView($('#negociacoesView'));
+
+        this._negociacoesView.update(this._listaNegociacoes);
     }
 
     adiciona(event) {
 
         event.preventDefault();
 
-        let data = DateHelper.textoParaData(this._inputData.value);
-
-        let negociacao = new Negociacao(
-            data,
-            this._inputQuantidade.value,
-            this._inputValor.value
-        );
-
-        this._listaNegociacoes.adiciona(negociacao);
+        this._listaNegociacoes.adiciona(this._criaNegociacao());
+        this._negociacoesView.update(this._listaNegociacoes);
         this._limpaFormulario();
 
+
         console.log(this._listaNegociacoes.negociacoes);
+    }
+
+    _criaNegociacao() {
+
+        return new Negociacao(
+                    DateHelper.textoParaData(this._inputData.value),
+                    this._inputQuantidade.value,
+                    this._inputValor.value
+                );            
     }
 
     _limpaFormulario() {
--- javascript-avancado-i-aula1/aluraframe/client/js/app/helpers/DateHelper.js
+++ javascript-avancado-i-aula1/aluraframe/client/js/app/helpers/DateHelper.js
@@ -7,9 +7,9 @@ class DateHelper {
 
     static textoParaData(texto) {
 
-        if(!/\d{4}-\d{2}-\d{2}/.test(texto)) 
+        if(!/^\d{4}-\d{2}-\d{2}$/.test(texto)) 
             throw new Error('Deve estar no formato aaaa-mm-dd');
-        
+
         return new Date(...texto.split('-').map((item, index) => item - index % 2));
     }
 
